5fe08a1674be57425e20b278884594a75efba177
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-23 11:48:58 -0600

    configurar servidor express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hola desde NodeJS');
+// Importación de paquetes
+const express = require('express');
+
+// Inicializar servidor express
+const app = express();
+
+// Definición de rutas
+app.get('/', (req, res) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'Todo salió bien',
+        uid: 1234
+    });
+});
+
+// Escuchar el servidor en el puerto 3000
+app.listen(3000, () => {
+    console.log(`Servidor HTTP coriendo en el puerto 3000`);
+});
