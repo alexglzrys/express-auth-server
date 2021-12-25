f5af9adae1787e03dd85f6933d054f8d65302565
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-25 16:34:56 -0600

    agregar SPA Angular en los archivos públicos del backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Importación de paquetes
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();     // Configurar y registrar nuestro archivo local de variables de entorno (a las variables de entorno globales de NODE)
 
@@ -19,6 +20,13 @@ app.use(express.json());        // lectura y parseo de información que viaje en
 
 // Importar Rutas
 app.use('/api/auth', require('./routes/auth'));
+
+// Servir nuestra SPA en cualquier otra ruta que no se encuentre registrado en este backend
+// Esto permite al router de Angular tener el control de todas sus rutas registradas
+// Esto se recomienda solo si el router de Angular tiene como configuración el useHash=false (por defecto)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
 // Escuchar el servidor en el puerto cuyo valor se encuentra en la variable PORT de nuestro archivo .env
 app.listen(process.env.PORT, () => {
